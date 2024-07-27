fba451711a2d6d2d548da6354f3a709d5d949b12
llloxim <erilu762@gmail.com>
2024-07-26 17:39:14 -0700

    added vircle effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import './App.css'
 import githubLogo from "./assets/github-mark.png";
 import linkedLogo from "./assets/LI-In-Bug.png";
+import React, { useState, useEffect } from 'react';
 function App() {
-
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  useEffect(() => {
+    const updateMousePosition = (e) => {
+      setX(e.clientX);
+      setY(e.clientY);
+    };
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
   return (
     <>
-      <h1>Eric Lu</h1>
-      <div className="card">  
-          <button>    
-          <a href="https://github.com/llloxim" target="_blank" rel="noopener noreferrer"><img src={githubLogo} width="100" height="100"></img ></a>
-          </button> 
-      </div>
-      <div className="card">
-        <button>
-          <a href="https://www.linkedin.com/in/eric-lu-10a66b215/" target="_blank" rel="noopener noreferrer">
-            <img src={linkedLogo} width="110" height="100"></img >
-            </a>
-        </button>
+      <div className="hover"
+      style={{
+
+        background: `radial-gradient(3.7em 4em at ${x}px ${y}px, #a6e7ed, transparent)`,
+      }}
+      > 
+        <div className='socials'>
+          <div className="card">  
+              <button>    
+                <a href="https://github.com/llloxim" target="_blank" rel="noopener noreferrer"><img src={githubLogo} width="100" height="100"></img ></a>
+              </button> 
+          </div>
+          <div className="card">
+            <button>
+              <a href="https://www.linkedin.com/in/eric-lu-10a66b215/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedLogo} width="110" height="100"></img >
+              </a>
+            </button>
+          </div>
+        </div>
+        <h1>Eric Lu</h1>
       </div>
     </>
   )
